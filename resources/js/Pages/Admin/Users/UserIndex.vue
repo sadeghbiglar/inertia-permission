a2158cbd65ab31a8d9dfec90399d5195896fea38
sadeghbiglar <sbmail555@gmail.com>
2023-10-25 21:19:11 +0330

    add user create form

--- resources/js/Pages/Admin/Users/UserIndex.vue
+++ resources/js/Pages/Admin/Users/UserIndex.vue
@@ -18,7 +18,13 @@ defineProps(['users'])
         </template>
 
         <div class="max-w-7xl mx-auto py-4">
-            <h1>Users Index Page</h1>
+            <div class="flex justify-between">
+                <h1>Users Index Page</h1>
+                <Link :href="route('users.create')" class="px-3 py-2 text-white bg-indigo-500 hover:bg-indigo-700 rounded">
+                    New User
+                </Link>
+            </div>
+
         </div>
         <div class="mt-6">
             <Table>
